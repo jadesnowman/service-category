10a4a9713aae69c30640e6f71d37525677489489
Ilham Saputrajati <ilhamsaputrajati@gmail.com>
2021-10-14 23:04:48 +0700

    store category

--- src/api/V1/controller/category.controller.js
+++ src/api/V1/controller/category.controller.js
@@ -1,12 +1,35 @@
+
+const db = require('../../../config/firedatabase');
+const { ref, set, update } = require('firebase/database');
 const { success, fail } = require('../../../helpers/response')
 
 const index = async (req, res, next) => {
+    res.json({
+        success: true
+    })
+    return;
+}
 
-    const msg = success([], "Data successfully retrieved!");
-    res.json(msg)
+const store = async (req, res, next) => {
+
+    const newCategoryName = new Date().getTime()
+    const categoryRef = ref(db, `/categories/${newCategoryName}`)
+
+    await set(categoryRef, {
+        name: req.body.name,
+        color: 'Dark',
+        quantity: req.body.quantity
+    })
+
+    await update(categoryRef, {
+        color: 'Yellow'
+    })
+
+    res.json(success(req.body.name, "Data successfully stored!"))
     return;
 }
 
 module.exports = {
-    index
+    index,
+    store
 }
--- src/api/V1/routes/category.route.js
+++ src/api/V1/routes/category.route.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const categoryController = require('../controller/category.controller')
 
 router.get("/", categoryController.index)
+router.post("/", categoryController.store)
 
 module.exports = router
